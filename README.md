19fead33ade651c25aef547fdd431f394a479424
Daniela Leguari <danielaleguari@outlook.com>
2023-06-05 15:25:27 -0300

    tipagem do observable

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Buscante
+# Projeto Buscante
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.3.
 
@@ -9,6 +9,10 @@ Run `ng serve --o`
 ## Padrão do projeto
 
 - A divisão do conteúdo: `Componentes de apresentação` (mais simples, que não possuem comportamento) e `componentes contêiner ou inteligentes` (que detêm informações e as enviam para outros componentes). Em inglês, chamamos ambos os tipos de dumb components e smart components, respectivamente.
+
+## API
+
+- Google Books APIs
 
 
 ## O que é RxJS?
--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class LivroService {
     private http: HttpClient
   ) { }
 
-  buscarLivros(valorDitado: string): Observable<any> {
+  buscarLivros(valorDitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDitado);
     return this.http.get(this.API, { params });
   }
