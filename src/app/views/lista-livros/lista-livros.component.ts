25f624162f2bbf9ecbee1ca019e701461452381e
Daniela Leguari <danielaleguari@outlook.com>
2023-06-06 14:25:15 -0300

    refatoração, classe livroVolumeInfo.ts

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,8 @@
-import { VolumeInfo } from './../../models/interfaces';
+import { Item, VolumeInfo } from './../../models/interfaces';
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -20,27 +21,16 @@ export class ListaLivrosComponent implements OnDestroy {
 
   buscarLivros() {
     this.subscription = this.service.buscarLivros(this.campoBusca).subscribe({
-      next: (items) => {this.listaLivros = this.livrosResultadoParaLivros(items)},
+      next: (items) => { this.listaLivros = this.livrosResultadoParaLivros(items) },
       error: erro => console.error(erro),
     }
     );
   }
 
-  livrosResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = []
-
-    items.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
-      })
-    });
-    return livros;
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item);
+    })
   }
 
   ngOnDestroy(): void {
