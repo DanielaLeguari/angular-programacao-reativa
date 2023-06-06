043a9581de3c6393133dc05197da5ae15654dfdb
Daniela Leguari <danielaleguari@outlook.com>
2023-06-06 13:40:54 -0300

    operador map

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { LivrosResultado } from '../models/interfaces';
+import { Item } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +14,14 @@ export class LivroService {
     private http: HttpClient
   ) { }
 
-  buscarLivros(valorDitado: string): Observable<LivrosResultado> {
+  buscarLivros(valorDitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDitado);
     return this.http.get<LivrosResultado>(this.API, {
       params
     }).pipe(
-      tap((retornoAPI) => console.log('Fluxo do tap', retornoAPI))
+      tap((retornoAPI) => console.log('Fluxo do tap', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('Fluxo após o map', resultado))
     )
   }
 }
